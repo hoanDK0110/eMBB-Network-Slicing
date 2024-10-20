e50ab7529db9e8487c2c43c778092d895c52d422
hoanDK0110 <hoan.dk0110@gmail.com>
2024-10-20 08:43:56 +0700

    2010

--- main.py
+++ main.py
@@ -5,48 +5,37 @@ import solving
 import benmark
 import numpy as np
 
-num_RUs = 3             # Số lượng RU (bao gồm RU ở tâm)
-num_DUs = 2             # Số lượng DU
-num_CUs = 1             # Số lượng CU
-num_UEs = 4             # Số lượng user eMBB
-radius_in = 100         # Bán kính vòng tròn trong (km)
-radius_out = 1000       # Bán kính vòng tròn ngoài (km)
-num_RBs = 2           # Số lượng của RBs
-num_antennas = 2        # Số lượng anntenas
+num_RUs = 4                             # Số lượng RU (bao gồm RU ở tâm)
+num_DUs = 2                             # Số lượng DU
+num_CUs = 2                             # Số lượng CU
+num_UEs = 10                             # Số lượng user
+radius_in = 100                         # Bán kính vòng tròn trong (km)
+radius_out = 1000                       # Bán kính vòng tròn ngoài (km)
+num_RBs = 2                             # Số lượng của RBs
+num_antennas = 8                        # Số lượng anntenas
 
-rb_bandwidth = 180e3    # Băng thông
+rb_bandwidth = 180e3                    # Băng thông
 # Maximum transmission power
-max_tx_power_dbm = 43   # dBm
+max_tx_power_dbm = 43                   # dBm
 max_tx_power_watts = 10**((max_tx_power_dbm)/10) # in mWatts  
-noise_power_watts = 1e-10 # Công suất nhiễu (in mWatts)
+noise_power_watts = 1e-10               # Công suất nhiễu (in mWatts)
 
 path_loss_ref = 128.1
 path_loss_exp = 37.6
 
-D_j = 30                # yêu cầu tài nguyên của node DU j
-D_m = 30                # yêu cầu tài nguyên của node CU m
+D_j = 10                                 # yêu cầu tài nguyên của node DU j
+D_m = 10                                 # yêu cầu tài nguyên của node CU m
 
-R_min = 1e6              # Data rate ngưỡng yêu cầu
+R_min = 1e6                             # Data rate ngưỡng yêu cầu
 
+epsilon = 1e-6
 
-# Khởi tạo biến
-# Khởi tạo ma trận z_bi_sk (biến nhị phân)
-z_bi_sk = np.zeros((num_RUs, num_UEs, num_RBs), dtype=int)
-
-# Khởi tạo ma trận phi_i_sk (biến nhị phân cho RU-UE)
-phi_i_sk = np.zeros((num_RUs, num_UEs), dtype=int)
-
-# Khởi tạo ma trận phi_j_sk (biến nhị phân cho DU-UE)
-phi_j_sk = np.zeros((num_DUs, num_UEs), dtype=int)
-
-# Khởi tạo ma trận phi_m_sk (biến nhị phân cho CU-UE)
-phi_m_sk = np.zeros((num_CUs, num_UEs), dtype=int)
-
-
-#Toạ toạ độ RU
+#Toạ toạ độ RU, UE
 coordinates_RU = gen_RU_UE.gen_coordinates_RU(num_RUs, radius_out)                  
-#Toạ toạ độ UE
 coordinates_UE = gen_RU_UE.gen_coordinates_UE(num_UEs, radius_in, radius_out) 
+
+# Tính khoảng cách giữa euclid RU-UE (km)
+distances_RU_UE = gen_RU_UE.calculate_distances(coordinates_RU, coordinates_UE, num_RUs, num_UEs)
 
 # Vẽ mạng wireless
 #graph = gen_RU_UE.plot_save_network(coordinates_RU, coordinates_UE, radius_in, radius_out)
@@ -57,17 +46,9 @@ G = RAN_topo.create_topo(num_RUs, num_DUs, num_CUs)
 
 # Danh sách tập các liên kết trong mạng
 l_ru_du, l_du_cu = RAN_topo.get_links(G)
-#print("l_ru_du: ", l_ru_du)
-#print("l_du_cu: ", l_du_cu)
 
 # Tập các capacity của các node DU, CU
 A_j, A_m = RAN_topo.get_node_cap(G)
-#print("A_j: ", A_j)
-#print("A_m: ", A_m)
-
-# Tính khoảng cách giữa euclid RU-UE (km)
-distances_RU_UE = gen_RU_UE.calculate_distances(coordinates_RU, coordinates_UE, num_RUs, num_UEs)
-#print("distances_RU_UE: ", distances_RU_UE)
 
 # Tính độ lợi của kênh truyền 
 gain = wireless.channel_gain(distances_RU_UE, num_RUs, num_UEs, num_RBs, noise_power_watts, num_antennas, path_loss_ref, path_loss_exp)
@@ -75,5 +56,8 @@ gain = wireless.channel_gain(distances_RU_UE, num_RUs, num_UEs, num_RBs, noise_p
 # Tính phân bổ công suất
 P_bi_sk = wireless.allocate_power(num_RUs, num_UEs, num_RBs, max_tx_power_watts)
 
-# Giải
-a, b = solving.optimize(num_UEs, num_RUs, num_DUs, num_CUs, num_RBs, max_tx_power_watts, rb_bandwidth, D_j, D_m, R_min, gain, P_bi_sk, A_j, A_m, l_ru_du, l_du_cu)  
+# Solve
+pi_sk, z_bi_sk, phi_i_sk, phi_j_sk, phi_m_sk = solving.optimize(num_UEs, num_RUs, num_DUs, num_CUs, num_RBs, max_tx_power_watts, rb_bandwidth, D_j, D_m, R_min, gain, P_bi_sk, A_j, A_m, l_ru_du, l_du_cu, epsilon) 
+
+# Show result
+benmark.print_mapping(pi_sk, z_bi_sk, phi_i_sk, phi_j_sk, phi_m_sk)
--- solving.py
+++ solving.py
@@ -1,77 +1,69 @@
 import numpy as np
 import cvxpy as cp
 
-def optimize(num_UEs, num_RUs, num_DUs, num_CUs, num_RBs, max_tx_power_watts, rb_bandwidth, D_j, D_m, R_min, gain, P_bi_sk, A_j, A_m, l_ru_du, l_du_cu):
+def optimize(num_UEs, num_RUs, num_DUs, num_CUs, num_RBs, max_tx_power_watts, rb_bandwidth, D_j, D_m, R_min, gain, P_bi_sk, A_j, A_m, l_ru_du, l_du_cu, epsilon):
     try:
-        z_bi_sk, phi_i_sk, phi_j_sk, phi_m_sk = {}, {}, {}, {}
-        # Biến phẩn bổ UE
-        for i in range(num_RUs):
-            for b in range(num_UEs):
-                for k in range(num_RBs):
-                    z_bi_sk[(i,k,b)]= cp.Variable(integer=True)
-
-        # Biến phẩn bổ RU
+        # Khởi tạo ma trận z_bi_sk (biến nhị phân)
+        z_bi_sk = {}
         for i in range(num_RUs):
             for k in range(num_UEs):
-                phi_i_sk[(i,k)]= cp.Variable(integer=True)
+                for b in range(num_RBs):
+                    z_bi_sk[(i, k, b)] = cp.Variable(boolean=True)
 
-        # Biến phẩn bổ E
-        for j in range(num_DUs):
-            for k in range(num_UEs):
-                phi_j_sk[(j, k)] = cp.Variable(integer=True) 
-
-        # Biến phẩn bổ CE
-        for m in range(num_CUs):
-            for k in range(num_UEs):
-                phi_m_sk[(m, k)] = cp.Variable(integer=True)  
+        # Khởi tạo các biến phi_i_sk, phi_j_sk, phi_m_sk
+        phi_i_sk = cp.Variable((num_RUs, num_UEs), boolean=True)
+        phi_j_sk = cp.Variable((num_DUs, num_UEs), boolean=True)
+        phi_m_sk = cp.Variable((num_CUs, num_UEs), boolean=True)
 
         # Biến tối ưu cho việc phân bổ
-        pi_sk = cp.Variable((num_RUs, num_UEs), integer=True)
-        # Hàm mục tiêu 
+        pi_sk = cp.Variable((num_UEs))
+
+        # Hàm mục tiêu
         objective = cp.Maximize(cp.sum(pi_sk))
 
         # Danh sách ràng buộc
         constraints = []
-        
+
         # Ràng buộc (15b)
         for k in range(num_UEs):
             R_sk = 0
             for b in range(num_RBs):
                 SNR = 0
                 for i in range(num_RUs): 
-                    SNR += P_bi_sk[i, k, b] * z_bi_sk[i, k, b] * gain[i, k, b]
-                R_sk += rb_bandwidth * cp.log(1 + SNR) / cp.log(2)
-            constraints.append(R_sk >= R_min * cp.sum(pi_sk[:, k]))
+                    SNR += P_bi_sk[i, k, b] * z_bi_sk[(i, k, b)] * gain[i, k, b]
+                R_sk += rb_bandwidth * cp.log(1 + SNR) / np.log(2)
+            constraints.append(R_sk >= R_min * cp.sum(pi_sk[k]))
 
         # Ràng buộc (15c)
         for i in range(num_RUs):
             total_power = 0
             for b in range(num_RBs):
                 for k in range(num_UEs):
-                    total_power += P_bi_sk[i, k, b] * z_bi_sk[i, k, b]
+                    total_power += P_bi_sk[i, k, b] * z_bi_sk[(i, k, b)]
             constraints.append(total_power <= max_tx_power_watts)
 
         # Ràng buộc (15d)
         for j in range(num_DUs):
-            count_du = cp.sum(phi_j_sk[j, :] * D_j)
+            count_du = cp.sum(phi_j_sk[j, :]) * D_j
             constraints.append(count_du <= A_j[j])
-        
+
         # Ràng buộc (15e)
         for m in range(num_CUs):
-            count_cu = cp.sum(phi_m_sk[m, :] * D_m)  
-            constraints.append(count_cu <= A_m[m]) 
+            count_cu = cp.sum(phi_m_sk[m, :]) * D_m
+            constraints.append(count_cu <= A_m[m])
 
         # Ràng buộc (15f + 15g + 15h)
         for k in range(num_UEs):
-            constraints.append(cp.sum(phi_i_sk[:, k]) == cp.sum(pi_sk[:, k]))
-            constraints.append(cp.sum(phi_j_sk[:, k]) == cp.sum(pi_sk[:, k]))
-            constraints.append(cp.sum(phi_m_sk[:, k]) == cp.sum(pi_sk[:, k]))
+            constraints.append(cp.sum(phi_i_sk[:, k]) == cp.sum(pi_sk[k]))
+            constraints.append(cp.sum(phi_j_sk[:, k]) == cp.sum(pi_sk[k]))
+            constraints.append(cp.sum(phi_m_sk[:, k]) == cp.sum(pi_sk[k]))
 
         # Ràng buộc (15i)
         for i in range(num_RUs):
             for k in range(num_UEs):
-                constraints.append(cp.sum(z_bi_sk[i, k, :]) <= phi_i_sk[i, k])  
-                constraints.append(cp.sum(z_bi_sk[i, k, :]) >= phi_i_sk[i, k])  
+                for b in range(num_RBs):
+                    constraints.append(z_bi_sk[(i, k, b)] <= phi_i_sk[i, k])
+                    constraints.append(phi_i_sk[i, k] <= z_bi_sk[(i, k, b)] + (1 - epsilon))
 
         # Ràng buộc (15j)
         for i in range(num_RUs):
@@ -87,18 +79,12 @@ def optimize(num_UEs, num_RUs, num_DUs, num_CUs, num_RBs, max_tx_power_watts, rb
         problem = cp.Problem(objective, constraints)
         problem.solve(solver=cp.MOSEK)
 
-        # Lưu trữ giá trị công suất sau khi tối ưu
-        P_bi_sk_values = np.zeros((num_RUs, num_UEs, num_RBs))
-        for i in range(num_RUs):
-            for b in range(num_RBs):
-                for k in range(num_UEs):
-                    P_bi_sk_values[i, k, b] = P_bi_sk[i, k, b].value
-
-        return P_bi_sk_values, pi_sk.value
+        return pi_sk, z_bi_sk, phi_i_sk, phi_j_sk, phi_m_sk
 
     except cp.SolverError:
         print('Solver error: non_feasible')
-        return None, None
+        return None, None, None, None, None
     except Exception as e:
         print(f'An error occurred: {e}')
-        return None, None
+        return None, None, None, None, None
+
